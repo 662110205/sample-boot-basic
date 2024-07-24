32aa8597aafb3d1d487b6a586d09651483655707
morjordor <151113683+662110205@users.noreply.github.com>
2024-07-24 15:52:25 +0700

    draft

---
+++ src/main/java/th/mfu/CustomerController.java
@@ -0,0 +1,55 @@
+package th.mfu;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.deser.std.CollectionDeserializer;
+import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
+
+@RestController
+public class CustomerController { 
+
+    @Autowired
+    private CustomerRepository custRepo;
+    // public static Map<Integer, Customer> customerDB = new HashMap<Integer, Customer>();
+    // private int nextId = 0;
+
+    //GET for a customeer
+
+    @GetMapping("/customers/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long id){
+        if(!custRepo.existsById(id))
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        Optional<Customer> customer = custRepo.findById(id);
+        return new ResponseEntity<Customer>(customer.get(), HttpStatus.OK);
+    }
+
+    @GetMapping("/customers")
+    public ResponseEntity<Collection> getAllCustomers(){
+        return new ResponseEntity<Collection>(HttpStatus.OK);
+    }
+
+    //POST for creating a customer 
+    @PostMapping("/customers")
+    public ResponseEntity<String> createCustomer(@RequestBody Customer customer){
+        custRepo.save(customer);
+        return new ResponseEntity<String>("Customer created", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("customers/{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable Long id){
+        custRepo.deleteById(id);
+        return new ResponseEntity<String>("Customer deleted", HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/th/mfu/ProductController.java
@@ -0,0 +1,47 @@
+package th.mfu;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
+
+public class ProductController {
+
+    @Autowired
+    private ProductRepository proRepo;
+
+    //GET for a customeer
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id){
+        if(!proRepo.existsById(id))
+            return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
+        Optional<Product> product = proRepo.findById(id);
+        return new ResponseEntity<Product>(product.get(), HttpStatus.OK);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<Collection> getAllProduct(){
+        return new ResponseEntity<Collection>(HttpStatus.OK);
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<String> createProduct(@RequestBody Product product){
+        proRepo.save(product);
+        return new ResponseEntity<String>("Product created", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("products/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id){
+        proRepo.deleteById(id);
+        return new ResponseEntity<String>("Product deleted", HttpStatus.NO_CONTENT);
+    }
+}
